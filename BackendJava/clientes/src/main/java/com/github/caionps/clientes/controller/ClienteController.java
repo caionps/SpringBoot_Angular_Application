27d36ea94aae96085520c8112df23083ca27dd49
caio.nepos <caio.nepos@gmail.com>
2022-05-31 10:17:11 -0300

    teste git

--- BackendJava/clientes/src/main/java/com/github/caionps/clientes/controller/ClienteController.java
+++ BackendJava/clientes/src/main/java/com/github/caionps/clientes/controller/ClienteController.java
@@ -12,6 +12,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
+    //testanto o git
 
     @Autowired
     private ClienteRepository repository;
